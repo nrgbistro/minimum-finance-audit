96994b0104a67f265046f2d802f42336cc8508fb
nrgbistro <nolangelinas@gmail.com>
2022-11-09 14:40:06 -0500

    Add data from shared repository

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,76 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const hardhat = require("hardhat");
+const { ethers } = hardhat;
+const {
+  deployVaultAndStrategy,
+  updateFenderVaults,
+  updateContractConfig,
+} = require("../utils/deployUtils.ts");
+const { addressBook } = require("blockchain-addressbook");
+const {
+  BOGUS_ADDR_1,
+  BOGUS_ADDR_3,
+  FHM_STAKER,
+  FHM_STAKE_MANAGER,
+} = require("../constants.js");
+
+const { spookyswap } = addressBook.fantom.platforms;
+
+const contractNames = {
+  vault: "MinimumVault",
+  strategy: "StrategyFantOHM",
+  vaultID: "fantohm-fhm",
+};
+
+const vaultConfig = {
+  name: "Minimum FantOHM",
+  symbol: "minFHM",
+  stratApprovalDelay: 21600,
+  wantCap: ethers.utils.parseUnits("50", 9),
+};
+
+const TESTNET_STAKER = "0x1cED6A6253388A56759da72F16D16544577D4dB7";
+const TESTNET_ROUTER = "0xcCAFCf876caB8f9542d6972f87B5D62e1182767d";
+const TESTNET_BURNER = "0x0ed9bB13cB4024555C7cC6374501e796b79e1a7f";
+
+const stratConfig = {
+  rebaseStaker: FHM_STAKER,
+  stakeManager: FHM_STAKE_MANAGER,
+  keeper: "0xC6cE315Ad5c636452aB28B4Dcd650C9212a4B92a",
+  unirouter: spookyswap.router,
+  serviceFeeRecipient: "0x1d116207f651bEdB7f05160B7Ac9b2Ce1A6b33B1",
+  minDeposit: 1000000,
+  discordLink: "https://discord.gg/fS5ZUwDtVK",
+};
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  await hardhat.run("compile");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const [deployer] = await ethers.getSigners();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  const deployed = await deployVaultAndStrategy(
+    contractNames,
+    vaultConfig,
+    stratConfig,
+    deployer
   );
+
+  const vault = deployed.vault;
+  const strategy = deployed.strategy;
+
+  console.log("Vault deployed to: ", vault.address);
+  console.log("Strategy deployed to: ", strategy.address);
+
+  //await updateFenderVaults(vault.address, contractNames.vaultID);
+  await updateContractConfig(vault.address, strategy.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+if (require.main === module) {
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
+
+module.exports = main;
